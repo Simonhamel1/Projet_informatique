36a4fec1201b7e694e7f45a92d6f506e6d938afc
Antoine <antoinelanglais388@gmail.com>
2024-05-10 18:16:31 +0200

    étape 3 programmation git dévévelloppenement pingoin new technono;ogie rsi 73

--- essaie.c
+++ essaie.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main(){
+    int pingouin;
     printf("salut");
     for(int i=0;i<45; i++){
         printf("1");
     }
     printf("Github c est nul ");
-    printf("Github c est nul ");
-    printf("Github c est nul ");
-    printf("Github c est nul ");
-    printf("Github c est nul ");
+    scanf("%d",&pingouin);
+    printf("le chiffre pingouin est :%d",pingouin);
+    
+    
     return 0;
 
 }
