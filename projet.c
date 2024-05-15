3df2dc586d87f78648e3765f0fd62f1659397197
Yaelsm19 <yaelsm19@gmail.com>
2024-05-15 20:30:59 +0200

    commentaire sur 3 fonctions et adaptation des variables dans les fonctions avec structure Game

--- projet.c
+++ projet.c
@@ -2,90 +2,98 @@
 #include <stdlib.h>
 #include <time.h>
 
-typedef struct{
-    int existence;
-    int nb_fish;
-    int penguin;
-}Box;
 
-typedef struct{
-    int nb_penguin;
-    char* name;
-    int score_player;
-}Player;
+typedef struct{    // Définition d'une structure représentant une case (Box) dans le jeu
+    int existence; // Indique si la case existe (1) ou non (0)
+    int nb_fish;   // Indique le nombre de poissons de la case
+    int penguin;   // Indique s'il y a un pingouin (1) ou non(0) sur cette case
+} Box;
 
-typedef struct{
-    int nb_player;
-    int nb_ligne;
-    int nb_column;
-    Player* player; 
-    Box** box;
-}Game;
 
-Box** creation_tableau(int nb_ligne, int nb_column) {
-    Box** tab = malloc(sizeof(Box*) * nb_ligne);
-    if(tab==NULL){
+typedef struct{        // Définition d'une structure représentant un joueur
+    int nb_penguin;    // Nombre de pingouins du joueur
+    char* name;        // Nom du joueur
+    int score_player;  // Score du joueur
+} Player;
+
+
+typedef struct{      // Définition d'une structure représentant l'état global du jeu
+    int nb_player;   // Nombre total de joueurs dans la partie
+    int nb_ligne;    // Nombre de lignes de la grille hexagonale
+    int nb_column;   // Nombre de colonnes de la grille hexagonale
+    Player* player;  // Tableau de structures Player représentant les différents joueurs
+    Box** box;       // Tableau à deux dimensions de structures Box représentant la grille de jeu hexagonale
+} Game;
+
+void creation_tableau(Game* game1) {   // Fonction pour créer et initialiser le tableau de jeu
+    Box** box1 = malloc(sizeof(Box*) * (*game1).nb_ligne);   // Allouer de la mémoire pour un tableau de pointeurs de Box
+    if (box1 == NULL) {    // Vérifier si l'allocation a échoué
         printf("erreur d'allocation mémoire 1");
-        exit(1);
+        exit(1); // Quitter le programme en cas d'erreur d'allocation
     }
-    for(int i=0; i<nb_ligne; i++){
-        tab[i] = malloc(sizeof(Box) * nb_column);
-        if(tab[i]==NULL){
-            printf("erreur d'allocation mémoire 2");
-            exit(1);
-        }
-    }
-    for(int j=0; j<nb_ligne; j++){
-        for(int k=0; k<nb_column; k++){
-                tab[j][k].existence = 1;
-        }
-    }
-    return tab;
-}
 
-void generer_poisson(Box** tab, int nb_ligne, int nb_column){
-    for(int i=0; i<nb_ligne; i++){
-        for(int j=0; j<nb_column; j++){
-            if((i!=(nb_ligne-1))||(j%2==0)){
-                tab[i][j].nb_fish = rand()%3 + 1;
+    for (int i = 0; i < (*game1).nb_ligne; i++) { // Allouer de la mémoire pour chaque ligne du tableau de Box
+        box1[i] = malloc(sizeof(Box) * (*game1).nb_column);
+        if (box1[i] == NULL) { // Vérifier si l'allocation a échoué pour chaque ligne
+            printf("erreur d'allocation mémoire 2");
+            exit(1); // Quitter le programme en cas d'erreur d'allocation
+        }
+    }
+    for (int j = 0; j < (*game1).nb_ligne; j++) {
+        for (int k = 0; k < (*game1).nb_column; k++) {
+            box1[j][k].existence = 1; // Initialiser le champ 'existence' à 1 pour chaque case
+        }
+    }
+    (*game1).box = box1;  // Affecter le tableau initialisé à la structure Game
+}
+void generer_poisson(Game* game1) {                               // Fonction pour générer aléatoirement des poissons dans les cases du tableau de jeu
+    for (int i = 0; i < game1->nb_ligne; i++) {
+        for (int j = 0; j < game1->nb_column; j++) {
+            if ((i != (game1->nb_ligne - 1)) || (j % 2 == 0)) {   // Vérifie la condition pour déterminer si des poissons doivent être générés dans cette case
+                game1->box[i][j].nb_fish = rand() % 3 + 1;        // Générer un nombre aléatoire de poissons entre 1 et 3 et l'affecter à la case actuelle
             }
         }
     }
 }
 
-void etat_poisson(Box** tab, int nb_ligne, int nb_column){
-    for(int i=0; i<nb_ligne; i++){
-        for(int j=0; j<nb_column; j++){
-                printf("%d ", tab[i][j].nb_fish);
+void etat_poisson(Game*  game1){
+    for(int i=0; i<game1->nb_ligne; i++){
+        for(int j=0; j<game1->nb_column; j++){
+            if(i==(game1->nb_ligne-1) && j%2==1){
+                printf("0 ", game1->box[i][j].nb_fish);
+            }
+            else{
+                printf("%d ", game1->box[i][j].nb_fish);
+            }
         }
         printf("\n");
     }
 }
-void afficher_grille(Box** tab, int nb_ligne, int nb_column){
-     for(int i=0; i<nb_column; i+=2){
+void afficher_grille(Game* game1){
+     for(int i=0; i<game1->nb_column; i+=2){
                 printf("  ____      ");
      }
      printf("\n");
-     for(int a=0; a<nb_ligne; a++){
+     for(int a=0; a<game1->nb_ligne; a++){
         int j=0;
         printf(" /");
-        while(j<nb_column){
-            if(tab[a][j].existence){
+        while(j<game1->nb_column){
+            if(game1->box[a][j].existence){
                     printf("    %c", 92);
             }
             else{
                 printf("xxxx%c", 92);
             }
             j++;
-            if(j<nb_column &&(a!=0 || j!=(nb_column-1))){
-                if(a!=0 && tab[a-1][j].existence==0){
+            if(j<game1->nb_column &&(a!=0 || j!=(game1->nb_column-1))){
+                if(a!=0 && game1->box[a-1][j].existence==0){
                     printf("xxxxxx/");
                 }
                 else{
-                    if(a!=0 && tab[a-1][j].nb_fish==3){
+                    if(a!=0 && game1->box[a-1][j].nb_fish==3){
                         printf("🐟 🐟 /");
                     }
-                    else if(a!=0 && tab[a-1][j].nb_fish==2){
+                    else if(a!=0 && game1->box[a-1][j].nb_fish==2){
                         printf("  🐟  /");
                     }
                     else{
@@ -98,9 +106,9 @@ void afficher_grille(Box** tab, int nb_ligne, int nb_column){
         printf("\n");
         int k=0;
         printf("/");
-        while(k<nb_column){
-            if(tab[a][k].existence==1){
-                if(tab[a][k].nb_fish>0){
+        while(k<game1->nb_column){
+            if(game1->box[a][k].existence==1){
+                if(game1->box[a][k].nb_fish>0){
                         printf(" 🐟   %c", 92);
                     }
                     else{
@@ -111,8 +119,8 @@ void afficher_grille(Box** tab, int nb_ligne, int nb_column){
                 printf("xxxxxx%c");
             }
             k++;
-            if(k<=(nb_column-1)){
-                if(a!=0 || k!=(nb_column-1)){
+            if(k<=(game1->nb_column-1)){
+                if(a!=0 || k!=(game1->nb_column-1)){
                     printf("____/");
                 }
                 else{
@@ -124,12 +132,12 @@ void afficher_grille(Box** tab, int nb_ligne, int nb_column){
         printf("\n");
         int l=0;
         printf("%c", 92);
-        while(l<nb_column){
-            if(tab[a][l].existence){
-                if(tab[a][l].nb_fish==3){
+        while(l<game1->nb_column){
+            if(game1->box[a][l].existence){
+                if(game1->box[a][l].nb_fish==3){
                     printf("🐟 🐟 /");
                 }
-                else if(tab[a][l].nb_fish==2){
+                else if(game1->box[a][l].nb_fish==2){
                     printf("   🐟 /");
                 }
                 else{
@@ -140,8 +148,8 @@ void afficher_grille(Box** tab, int nb_ligne, int nb_column){
                 printf("xxxxxx/");
             }
             l++;
-            if(l<nb_column && (a!=(nb_ligne-1) || l!=(nb_column-1))){
-                if(tab[a][l].existence){
+            if(l<game1->nb_column && (a!=(game1->nb_ligne-1) || l!=(game1->nb_column-1))){
+                if(game1->box[a][l].existence){
                     printf("    %c", 92);
                 }
                 else{
@@ -153,12 +161,12 @@ void afficher_grille(Box** tab, int nb_ligne, int nb_column){
         printf("\n");
         int m=0;
         printf(" %c", 92);
-        while(m<nb_column){
+        while(m<game1->nb_column){
             printf("____/");
             m++;
-            if(m<nb_column && (a!=(nb_ligne-1) || m!=(nb_column-1))){
-                if(tab[a][m].existence){
-                    if(tab[a][m].nb_fish>0 && a!=(nb_ligne-1)){
+            if(m<game1->nb_column && (a!=(game1->nb_ligne-1) || m!=(game1->nb_column-1))){
+                if(game1->box[a][m].existence){
+                    if(game1->box[a][m].nb_fish>0 && a!=(game1->nb_ligne-1)){
                         printf("  🐟  %c", 92);
                     }
                     else{
@@ -173,30 +181,32 @@ void afficher_grille(Box** tab, int nb_ligne, int nb_column){
         }
         printf("\n");
     }}
-void create_dimension(Game* game1){
-    int number_players = (*game1).nb_player;
-    int number_ligne;
-    int number_penguin;
-    if(number_players==2){
+//void create_dimension(Game* game1){
+ //   int number_players = (*game1).nb_player;
+  //  int number_ligne;
+   // int number_penguin;
+    //if(number_players==2){
 
-    }
-    else if(number_players==2){
+   /// }
+    //else if(number_players==2){
 
-    }
-    else if(number_players==2){
+    //}
+    //else if(number_players==2){
 
-    }
-    else if(number_players==2){
+    //}
+    //else if(number_players==2){
 
-    }
-}
+    //}
+//}
 int main() {
     srand(time(NULL));
     system("chcp 65001");
-    Game game;
-    Box** box1 = creation_tableau(4, 7);
-    game.box = box1;
-    generer_poisson(game.box, 4, 7);
-    afficher_grille(game.box, 4, 7);
+    Game game1;
+    Game* pointer_game1 = &game1;
+    game1.nb_ligne = 4;
+    game1.nb_column = 7;
+    creation_tableau(pointer_game1);
+    generer_poisson(pointer_game1);
+    afficher_grille(pointer_game1);
     return 0;
 }
