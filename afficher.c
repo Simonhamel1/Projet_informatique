749a4a601bc2fc67ca8fe7284a2428f812979786
Antoine <antoinelanglais388@gmail.com>
2024-05-24 14:12:03 +0200

    Commentaire sur fonction afficher.c 👍

--- afficher.c
+++ afficher.c
@@ -13,194 +13,197 @@
 #define CYAN "\033[36m"
 #define WHITE "\033[37m"
 
-int search_coordinate_in_tab_penguin(int i, int j, Game* game1){
-    for(int k=0; k<game1->nb_player; k++){
-        for(int l=0; l<game1->player[k].nb_penguin; l++){
-            if(game1->player[k].tab_penguin[l].x == i && game1->player[k].tab_penguin[l].y == j){
-                return k+1;
+int search_coordinate_in_tab_penguin(int i, int j, Game* game1){ // Définition de la fonction pour rechercher les coordonnées dans le tableau de pingouins
+    for(int k=0; k<game1->nb_player; k++){ // Parcours des joueurs
+        for(int l=0; l<game1->player[k].nb_penguin; l++){ // Parcours des pingouins de chaque joueur
+            if(game1->player[k].tab_penguin[l].x == i && game1->player[k].tab_penguin[l].y == j){ // Si les coordonnées correspondent à celles d'un pingouin
+                return k+1; // Retourner l'indice du joueur
             }
         }
     }
-    return 0;
+    return 0; // Si les coordonnées ne correspondent à aucun pingouin, retourner 0
 }
-char* give_num_penguin(int num_player, int i, int j, Game* game1){
-    if(num_player!=0){
-        for(int k=0; k<game1->player[num_player-1].nb_penguin; k++){
-                if(game1->player[num_player-1].tab_penguin[k].x == i && game1->player[num_player-1].tab_penguin[k].y == j){
-                    switch(k){
+char* give_num_penguin(int num_player, int i, int j, Game* game1){ // Définition de la fonction pour donner le numéro du pingouin
+    if(num_player!=0){ // Vérification si le numéro du joueur est différent de 0
+        for(int k=0; k<game1->player[num_player-1].nb_penguin; k++){ // Parcours des pingouins du joueur spécifié
+                if(game1->player[num_player-1].tab_penguin[k].x == i && game1->player[num_player-1].tab_penguin[k].y == j){ // Si les coordonnées correspondent à celles d'un pingouin du joueur
+                    switch(k){ // Switch basé sur l'indice du pingouin
                         case 0 :
-                            return "1";
+                            return "1"; // Retourne "1" pour le premier pingouin
                             break;
                         case 1 :
-                            return "2";
+                            return "2"; // Retourne "2" pour le deuxième pingouin
                             break;
                         case 2 :
-                            return "3";
+                            return "3"; // Retourne "3" pour le deuxième pingouin
                             break;
                         case 3 :
-                            return "4";
+                            return "4"; // Retourne "4" pour le deuxième pingouin
                             break;
-                        default :
-                            return ".";
+                        default :  // Cas par défaut (si l'indice est supérieur à 3)
+                            return "."; // Retourne "." pour indiquer un pingouin non spécifié
                     }
                 }
         }
     }
 }
-char* give_caractere_penguin(int i, int j, Game* game1){
-    char* a = (char*)malloc(30 * sizeof(char));
-    if (a == NULL) {
-        fprintf(stderr, "Erreur d'allocation mémoire\n");
-        exit(1);
+char* give_caractere_penguin(int i, int j, Game* game1){ // fonction pour donner le caractère représentant un pingouin
+    char* a = (char*)malloc(30 * sizeof(char)); // Allocation dynamique d'un espace mémoire pour stocker le caractère
+    if (a == NULL) { // Vérification si l'allocation a échoué
+        fprintf(stderr, "Erreur d'allocation mémoire\n"); // Affichage d'un message d'erreur
+        exit(1); // Quitter le programme en cas d'erreur d'allocation
     }
 
-    int num_player = search_coordinate_in_tab_penguin(i, j, game1);
+    int num_player = search_coordinate_in_tab_penguin(i, j, game1); // Recherche du numéro du joueur à cette position
 
-    if (num_player == 0) {
-        strcpy(a, "    ");
+    if (num_player == 0) { // Si aucun pingouin n'est présent à cette position
+        strcpy(a, "    "); // Définir une chaîne vide pour représenter une case vide
     } 
-    else {
-        char* color;
-        switch (num_player) {
-            case 1: color = RED; break;
-            case 2: color = YELLOW; break;
-            case 3: color = GREEN; break;
-            case 4: color = BLUE; break;
-            case 5: color = CYAN; break;
-            case 6: color = WHITE; break;
-            default:
-                free(a);
-                exit(1);
+    else { // Si un pingouin est présent à cette position
+        char* color; // Déclaration d'une variable pour stocker la couleur
+        switch (num_player) { // Switch basé sur le numéro du joueur
+            case 1: color = RED; break; // Si le joueur est le joueur 1, couleur rouge
+            case 2: color = YELLOW; break; // Si le joueur est le joueur 2, couleur jaune
+            case 3: color = GREEN; break; // Si le joueur est le joueur 3, couleur verte
+            case 4: color = BLUE; break; // Si le joueur est le joueur 4, couleur bleue
+            case 5: color = CYAN; break; // Si le joueur est le joueur 5, couleur cyan
+            case 6: color = WHITE; break; // Si le joueur est le joueur 6, couleur blanche
+            default: // Cas par défaut (numéro de joueur non valide) 
+                free(a); // Libération de la mémoire allouée précédemment
+                exit(1); // Quitter le programme en cas d'erreur
         }
         
-        strcpy(a, color);
-        strcat(a, "🐧P");
-        strcat(a, give_num_penguin(num_player, i, j, game1));
-        strcat(a, RESET);
+        strcpy(a, color); // Copie de la couleur dans la chaîne
+        strcat(a, "🐧P"); // Ajout du symbole de pingouin à la chaîne
+        strcat(a, give_num_penguin(num_player, i, j, game1)); // Ajout du numéro du pingouin à la chaîne
+        strcat(a, RESET);  // Réinitialisation de la couleur après le pingouin
     }
-    return a;
+    return a;  // Retourne la chaîne représentant le pingouin
 }
-void afficher_grille(Game* game1){
-     for(int i=0; i<game1->nb_column; i+=2){
-                printf("  ____      ");
+void afficher_grille(Game* game1){  // Procèdure  pour afficher le plateau de jeu
+     for(int i=0; i<game1->nb_column; i+=2){ // Boucle pour afficher la ligne supérieure de la grille
+                printf("  ____      "); // Affiche les bords de la grille
      }
-     printf("\n");
-     for(int a=0; a<game1->nb_ligne; a++){
+     printf("\n"); // Passage à la ligne suivante
+     for(int a=0; a<game1->nb_ligne; a++){ // Boucle pour parcourir chaque ligne de la grille
         int j=0;
-        printf(" /");
-        while(j<game1->nb_column){
-            if(game1->box[a][j].existence){
-                    printf("    %c", 92);
+        printf(" /"); // Affiche le bord gauche de la grille
+        while(j<game1->nb_column){ // Boucle pour parcourir chaque colonne de la grille
+            if(game1->box[a][j].existence){ // Vérifie si la case existe
+                    printf("    %c", 92); // Affiche une case vide
             }
             else{
-                printf("xxxx%c", 92);
+                printf("xxxx%c", 92); // Affiche une case bloquée
             }
             j++;
-            if(j<game1->nb_column &&(a!=0 || j!=(game1->nb_column-1))){
+            if(j<game1->nb_column &&(a!=0 || j!=(game1->nb_column-1))){ // Vérifie si une ligne est à afficher après la case
                 if(a!=0 && game1->box[a-1][j].existence==0){
-                    printf("xxxxxx/");
+                    printf("xxxxxx/"); // Affiche une ligne de cases bloquées
                 }
                 else{
                     if(a!=0 && game1->box[a-1][j].penguin==1){
-                        printf("      /");
+                        printf("      /"); // Affiche une ligne de cases avec des pingouins
                     }
                     else if(a!=0 && game1->box[a-1][j].nb_fish==3){
-                        printf("🐟 🐟 /");
+                        printf("🐟 🐟 /"); // Affiche une ligne de cases avec deux poissons
                     }
                     else if(a!=0 && game1->box[a-1][j].nb_fish==2){
-                        printf("  🐟  /");
+                        printf("  🐟  /"); // Affiche une ligne de cases avec un poisson
                     }
                     else{
-                        printf("      /");
+                        printf("      /"); // Affiche une ligne de cases vides
                     }
                 }
             }
             j++;
         }
-        printf("\n");
+        printf("\n"); // Passage à la ligne suivante
         int k=0;
-        printf("/");
-        while(k<game1->nb_column){
-            if(game1->box[a][k].existence==1){
+        printf("/"); // Affiche le bord gauche de la grille
+        while(k<game1->nb_column){ // Boucle pour parcourir chaque colonne de la grille
+            if(game1->box[a][k].existence==1){ // Vérifie si la case existe
                 if(game1->box[a][k].penguin ==1){
-                    printf(" %s %c", give_caractere_penguin(a, k, game1),92);
+                    printf(" %s %c", give_caractere_penguin(a, k, game1),92); // Affiche un pingouin dans une case
                 }
                 else if(game1->box[a][k].nb_fish>0){
-                        printf(" 🐟   %c", 92);
+                        printf(" 🐟   %c", 92);  // Affiche un poisson dans une case
+                    }
                     }
                 else{
-                    printf(" %s %c", give_caractere_penguin(a, k, game1),92);
+                    printf(" %s %c", give_caractere_penguin(a, k, game1),92); // Affiche une case vide ou bloquée
                 }
             }
             else{
-                printf("xxxxxx%c");
+                printf("xxxxxx%c"); // Affiche une case bloquée
             }
             k++;
-            if(k<=(game1->nb_column-1)){
-                if(a!=0 || k!=(game1->nb_column-1)){
-                    printf("____/");
+            if(k<=(game1->nb_column-1)){ // Vérifie s'il y a une ligne à afficher après la case
+                if(a!=0 || k!=(game1->nb_column-1)){ 
+                    printf("____/"); // Affiche une ligne horizontale
                 }
                 else{
-                    printf("____");
+                    printf("____"); // Affiche une ligne horizontale
                 }
             }
             k++;
         }
-        printf("\n");
+        printf("\n"); // Passage à la ligne suivante
         int l=0;
-        printf("%c", 92);
-        while(l<game1->nb_column){
+        printf("%c", 92); // Affiche le bord gauche de la grille
+        while(l<game1->nb_column){ // Boucle pour parcourir chaque colonne de la grille
             if(game1->box[a][l].existence){
                 if(game1->box[a][l].penguin==1){
-                    printf("      /");
+                    printf("      /");  // Affiche une ligne de cases avec des pingouins
+                }
                 }
                 else if(game1->box[a][l].nb_fish==3){
-                    printf("🐟 🐟 /");
+                    printf("🐟 🐟 /"); // Affiche une ligne de cases avec deux poissons
+                }
                 }
                 else if(game1->box[a][l].nb_fish==2){
-                    printf("   🐟 /");
+                    printf("   🐟 /"); // Affiche une ligne de cases avec un poisson
                 }
                 else{
-                   printf("      /"); 
+                   printf("      /");  // Affiche une ligne de cases vides
                 }
             }
             else{
-                printf("xxxxxx/");
+                printf("xxxxxx/"); // Affiche une ligne de cases bloquées
             }
             l++;
-            if(l<game1->nb_column && (a!=(game1->nb_ligne-1) || l!=(game1->nb_column-1))){
+            if(l<game1->nb_column && (a!=(game1->nb_ligne-1) || l!=(game1->nb_column-1))){ // Vérifie s'il y a une ligne à afficher après la case
                 if(game1->box[a][l].existence){
-                    printf("    %c", 92);
+                    printf("    %c", 92);  // Affiche une ligne de cases vides ou avec des pingouins
                 }
                 else{
-                    printf("xxxx%c", 92);
+                    printf("xxxx%c", 92); // Affiche une ligne de cases bloquées
                 }
             }
             l++;
         }
-        printf("\n");
-        int m=0;
-        printf(" %c", 92);
-        while(m<game1->nb_column){
-            printf("____/");
-            m++;
-            if(m<game1->nb_column && (a!=(game1->nb_ligne-1) || m!=(game1->nb_column-1))){
-                if(game1->box[a][m].existence){
+        printf("\n"); // Passage à la ligne suivante
+        int m=0; // Initialisation de la variable de boucle pour parcourir les colonnes
+        printf(" %c", 92);  // Affiche le bord gauche de la grille
+        while(m<game1->nb_column){ // Boucle pour parcourir chaque colonne de la grille
+            printf("____/"); // Affiche une colonne de séparateurs verticaux
+            m++;  // Passage à la colonne suivante
+            if(m<game1->nb_column && (a!=(game1->nb_ligne-1) || m!=(game1->nb_column-1))){ // Vérifie s'il y a une colonne à afficher après la case
+                if(game1->box[a][m].existence){ // Vérifie si la case existe
                     if(a!=(game1->nb_ligne-1) && game1->box[a][m].penguin==1){
-                        printf(" %s %c", give_caractere_penguin(a, m, game1), 92);
+                        printf(" %s %c", give_caractere_penguin(a, m, game1), 92); // Affiche un pingouin dans une case
                     }
                     else if(game1->box[a][m].nb_fish>0 && a!=(game1->nb_ligne-1)){
-                        printf("  🐟  %c", 92);
+                        printf("  🐟  %c", 92); // Affiche un poisson dans une case
                     }
                     else{
-                       printf("      %c", 92);
+                       printf("      %c", 92); // Affiche une case vide ou bloquée
                     }
                 }
                 else{
-                    printf("xxxxxx%c", 92);
+                    printf("xxxxxx%c", 92); // Affiche une case bloquée
                 }
             }
-            m++;
+            m++; // Passage à la colonne suivante
         }
-        printf("\n");
+        printf("\n"); // Passage à la ligne suivante
     }}
