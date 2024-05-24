9d1226c1132961a41ed08d3af24bde8f137f2df0
Antoine <antoinelanglais388@gmail.com>
2024-05-24 12:20:51 +0200

    Terminer commentaires fichier tour_joueur.c

--- tour_joueur.c
+++ tour_joueur.c
@@ -226,122 +226,122 @@ void one_move(int choix_deplacement, int num_penguin, int num_player, Game* game
     
     }
 }
-int verify_choice_penguin(int num_penguin, int num_player, Game* game1){
-    int i = game1->player[num_player-1].tab_penguin[num_penguin-1].x;
+int verify_choice_penguin(int num_penguin, int num_player, Game* game1){  // Fonction qui vérifie si un pingouin d'un joueur donné peut effectuer un mouvement valide
+    int i = game1->player[num_player-1].tab_penguin[num_penguin-1].x; // Récupère les coordonnées actuelles du pingouin
     int j = game1->player[num_player-1].tab_penguin[num_penguin-1].y;
-    if(verify_haut_gauche(i, j, game1) || verify_haut_droite(i, j, game1) || verify_haut(i, j, game1) || verify_bas_gauche(i, j, game1) || verify_bas(i, j, game1) || verify_bas_droite(i, j, game1)){
-        return 1;
+    if(verify_haut_gauche(i, j, game1) || verify_haut_droite(i, j, game1) || verify_haut(i, j, game1) || verify_bas_gauche(i, j, game1) || verify_bas(i, j, game1) || verify_bas_droite(i, j, game1)){ // Vérifie si le pingouin peut se déplacer dans une des six directions possibles
+        return 1; // Retourne 1 si un mouvement  est possible
     }
-    return 0;
+    return 0; // Retourne 0 si aucun mouvement  n'est possible
 }
-int verify_all_penguin_one_player(int num_player, Game* game1){
-    for(int i=0; i<game1->player[num_player-1].nb_penguin; i++){
-        if(verify_choice_penguin(i+1, num_player, game1)){
-            return 1;
+int verify_all_penguin_one_player(int num_player, Game* game1){ // Fonction qui vérifie si au moins un des pingouins d'un joueur donné peut effectuer un mouvement 
+    for(int i=0; i<game1->player[num_player-1].nb_penguin; i++){ // Boucle  pour vérifier chaque  pingouins du joueur
+        if(verify_choice_penguin(i+1, num_player, game1)){ // Vérifie si le pingouin actuel peut se déplacer
+            return 1;  // Retourne 1 si au moins un pingouin peut se déplacer
         }
     }
-    return 0;
+    return 0; // Retourne 0 si aucun pingouin ne peut se déplacer
 }
-int verify_all_penguin_all_player(Game* game1){
-    for(int i=0; i<game1->nb_player; i++){
-        if(verify_all_penguin_one_player(i+1, game1)==1){
-            return 1;
+int verify_all_penguin_all_player(Game* game1){ // Fonction qui vérifie si au moins un des pingouins de tous les joueurs peut effectuer un mouvement 
+    for(int i=0; i<game1->nb_player; i++){ // Boucle qui parcourt tous les joueurs
+        if(verify_all_penguin_one_player(i+1, game1)==1){ // Vérifie si au moins un pingouin du joueur actuel peut se déplacer
+            return 1; // Retourne 1 si au moins un pingouin de n'importe quel joueur peut se déplacer
         }
     }
-    return 0;
+    return 0; // Retourne 0 si aucun pingouin de tous les joueurs ne peut se déplacer
 }
-int verify_choix_direction(int choix_direction, int i, int j, Game* game1){
-    switch(choix_direction){
-        case 1:
-            return verify_haut_gauche(i, j, game1);
-        case 2:
-            return verify_haut(i, j, game1);
-        case 3:
-            return verify_haut_droite(i, j, game1);
-        case 4:
-            return verify_bas_gauche(i, j, game1);
-        case 5:
-            return verify_bas(i, j, game1);
-        case 6:
-            return verify_bas_droite(i, j, game1);
+int verify_choix_direction(int choix_direction, int i, int j, Game* game1){ // Fonction qui vérifie si un mouvement dans une direction choisie est possible à partir d'une case donnée
+    switch(choix_direction){  // Sélectionne la direction du mouvement en fonction de l'entrée choix_direction
+        case 1: // Si la direction choisie est 1 (haut-gauche)
+            return verify_haut_gauche(i, j, game1);// Vérifie si le mouvement haut-gauche est possible
+        case 2: // Si la direction choisie est 2 (haut)
+            return verify_haut(i, j, game1); // Vérifie si le mouvement vers le haut est possible
+        case 3: // Si la direction choisie est 3 (haut-droite)
+            return verify_haut_droite(i, j, game1); // Vérifie si le mouvement haut-droite est possible
+        case 4: // Si la direction choisie est 4 (bas-gauche)
+            return verify_bas_gauche(i, j, game1); // Vérifie si le mouvement bas-gauche est possible
+        case 5: // Si la direction choisie est 5 (bas)
+            return verify_bas(i, j, game1); // Vérifie si le mouvement vers le bas est possible
+        case 6: // Si la direction choisie est 6 (bas-droite)
+            return verify_bas_droite(i, j, game1); // Vérifie si le mouvement bas-droite est possible
     }
 }
-int choose_direction(int num_penguin, int num_player, Game* game1){
-        int verify = 1;
-        int choix_direction;
-        int i = game1->player[num_player-1].tab_penguin[num_penguin-1].x;
+int choose_direction(int num_penguin, int num_player, Game* game1){ // Fonction qui permet à un joueur de choisir une direction de déplacement pour un pingouin
+        int verify = 1; // Variable de contrôle pour valider le choix de la direction
+        int choix_direction; // Variable pour stocker la direction choisie
+        int i = game1->player[num_player-1].tab_penguin[num_penguin-1].x; // Récupère les coordonnées  actuelle du pingouin
         int j = game1->player[num_player-1].tab_penguin[num_penguin-1].y;
-        do{
-        printf("choisi la direction vers ou tu veux le déplacer :\n");
-        printf("1: haut_gauche, 2: haut, 3: haut_droite, 4: bas_gauche, 5: bas, 6: bas_droite\n");
-        scanf("%d", &choix_direction);
-        if(choix_direction<1 || choix_direction>6){
-            printf("choississez un bon numero");
+        do{  // Boucle pour répéter l'instruction  jusqu'à ce qu'une direction valide soit choisie
+        printf("choisi la direction vers ou tu veux le déplacer :\n"); // Invite  l'utilisateur à choisir une direction
+        printf("1: haut_gauche, 2: haut, 3: haut_droite, 4: bas_gauche, 5: bas, 6: bas_droite\n"); // Affiche les options de direction
+        scanf("%d", &choix_direction); // Lit la saisie  de l'utilisateur
+        if(choix_direction<1 || choix_direction>6){ // Vérifie si l'entrée est une direction valide
+            printf("choississez un bon numero"); // Demande à l'utilisateur de choisir un numéro valide
         }
-        else{
-            if(verify_choix_direction(choix_direction, i, j, game1)==0){
-                printf("ce pingouin ne peux pas se deplacer dans cette direction");
+        else{ // Si la direction choisie est valide
+            if(verify_choix_direction(choix_direction, i, j, game1)==0){ // Vérifie si le pingouin peut se déplacer dans la direction choisie
+                printf("ce pingouin ne peux pas se deplacer dans cette direction"); // Informe l'utilisateur que le pingouin ne peut pas se déplacer dans cette direction
             }
-            else{
-                verify = 0;
+            else{ // Si le pingouin peut se déplacer dans la direction choisie
+                verify = 0; // Met à jour la variable de contrôle pour sortir de la boucle
             }
         }
-        }while(verify);
-        return choix_direction;
+        }while(verify); // Répète la boucle tant qu'une direction valide n'est pas choisie
+        return choix_direction; // Retourne la direction choisie
 }
-int verify_nombre_deplacement(int nombre_deplacement, int choix_deplacement, int num_penguin, int num_player, Game* game1){
-    int i = game1->player[num_player-1].tab_penguin[num_penguin-1].x;
+int verify_nombre_deplacement(int nombre_deplacement, int choix_deplacement, int num_penguin, int num_player, Game* game1){ // Fonction qui vérifie si un nombre de déplacements dans une direction donnée est possible pour un pingouin
+    int i = game1->player[num_player-1].tab_penguin[num_penguin-1].x; // Récupère les coordonnées  actuelle du pingouin
     int j = game1->player[num_player-1].tab_penguin[num_penguin-1].y;
-    int verify = 1;
-    if(nombre_deplacement<0){
-        printf("nombre de deplacement incorrecte\n");
-        return 0;
+    int verify = 1; // Variable de contrôle pour vérifier la validité des déplacements
+    if(nombre_deplacement<0){ // Vérifie si le nombre de déplacements est incorrect (négatif)
+        printf("nombre de deplacement incorrecte\n"); // Si incorrect affiche un message d'erreur
+        return 0; // Retourne 0 pour indiquer que les déplacements ne sont pas valides
     }
-    for(int k=0; k<nombre_deplacement; k++){
-        verify = verify_choix_direction(choix_deplacement, i, j, game1);
-        if(verify == 0){
-            return 0;
-        }
-        else{
-            switch(choix_deplacement){
-            case 1:
-                if(j%2==0){
+    for(int k=0; k<nombre_deplacement; k++){ // Boucle pour vérifier chaque déplacement
+        verify = verify_choix_direction(choix_deplacement, i, j, game1); // Vérifie si le déplacement dans la direction choisie est possible
+        if(verify == 0){ // Si le déplacement n'est pas possible
+            return 0; // Retourne 0 pour indiquer que les déplacements ne sont pas valides
+        } 
+        else{ // Si le déplacement est possible
+            switch(choix_deplacement){ // Met à jour les coordonnées en fonction de la direction choisie
+            case 1: // Déplacement haut_gauche
+                if(j%2==0){ // Si la colonne est paire
                     i--;
                     j--;
                 }
-                else{
+                else{ // Si la colonne est impaire
                     j--;
                 }
                 break;
-            case 2:
+            case 2: // Déplacement haut
                 i--;
                 break;
-            case 3:
-                if(j%2==0){
+            case 3: // Déplacement haut_droite
+                if(j%2==0){ // Si la colonne est paire
                     i--;
                     j++;
                 }
-                else{
+                else{ // Si la colonne est impaire
                     j++;
                 }
                 break;
-            case 4:
-                if(j%2==0){
+            case 4: // Déplacement bas_gauche
+                if(j%2==0){ // Si la colonne est paire
                     j--;
                 }
-                else{
+                else{ // Si la colonne est impaire
                     i++;
                     j--;
                 }
                 break;
-            case 5:
+            case 5: // Déplacement bas
                 i++;
                 break;
-            case 6:
-                if(j%2==0){
+            case 6: // Déplacement bas_droite
+                if(j%2==0){ // Si la colonne est paire
                     j++;
                 }
-                else{
+                else{ // Si la colonne est impaire
                     i++;
                     j++;
                 }
@@ -351,81 +351,81 @@ int verify_nombre_deplacement(int nombre_deplacement, int choix_deplacement, int
         
         
     }
-    return verify;
+    return verify; // Retourne la variable de contrôle pour indiquer la validité des déplacements
 }
-void move_total(int nombre_deplacement, int num_penguin, int choix_deplacement, int num_player, Game* game1){
-    for(int i=0; i<nombre_deplacement; i++){
-        one_move(choix_deplacement, num_penguin, num_player, game1);
+void move_total(int nombre_deplacement, int num_penguin, int choix_deplacement, int num_player, Game* game1){ // Procédure qui déplace un pingouin d'un joueur un nombre donné de fois dans une direction choisie sur le plateau de jeu
+    for(int i=0; i<nombre_deplacement; i++){ // Boucle pour effectuer le nombre de déplacements spécifié
+        one_move(choix_deplacement, num_penguin, num_player, game1); // Appelle la fonction one_move pour déplacer le pingouin dans la direction choisie
     }
 }
-void one_player_tour(int num_player, Game* game1){
-    int num_penguin;
-    int choix_direction;
-    int nombre_deplacement;
-    int verify = 1;
-    int score = game1->player[num_player-1].nb_penguin;
-    if(verify_all_penguin_one_player(num_player, game1)==0){
-        printf("tout tes pingouins sont bloques joueur %d, passage au joueur suivant\n", num_player);
+void one_player_tour(int num_player, Game* game1){  // Procédure qui gère le tour d'un joueur spécifique dans le jeu
+    int num_penguin; // Variable pour le numéro du pingouin à déplacer
+    int choix_direction; // Variable pour la direction choisie du déplacement
+    int nombre_deplacement; // Variable pour le nombre de déplacements souhaités
+    int verify = 1; // Variable pour vérifier la validité des choix du joueur
+    int score = game1->player[num_player-1].nb_penguin; // Sauvegarde le score initial du joueur
+    if(verify_all_penguin_one_player(num_player, game1)==0){ // Vérifie si tous les pingouins du joueur sont bloqués
+        printf("tout tes pingouins sont bloques joueur %d, passage au joueur suivant\n", num_player); // Affiche un message si tous les pingouins du joueur sont bloqués
     }
     else{
         do{
-        printf("quelle pingouin souhaite tu déplacer joueur %d,  ", num_player);
-        afficher_coordinate_one_penguin(num_player, game1);
+        printf("quelle pingouin souhaite tu déplacer joueur %d,  ", num_player); // Demande au joueur de choisir un pingouin à déplacer
+        afficher_coordinate_one_penguin(num_player, game1); // Affiche les coordonnées des pingouins du joueur
         printf(" :\n");
-        scanf("%d", &num_penguin);
-        if(num_penguin<1 || num_penguin>(game1->player[num_player-1].nb_penguin)){
-            printf("le numero du pingouin n'est pas bon\n");
+        scanf("%d", &num_penguin); // Lit le numéro du pingouin choisi
+        if(num_penguin<1 || num_penguin>(game1->player[num_player-1].nb_penguin)){ // Vérifie si le numéro du pingouin est valide
+            printf("le numero du pingouin n'est pas bon\n"); // Affiche un message d'erreur si le numéro est invalide
         }
         else{
-            if(verify_choice_penguin(num_penguin, num_player, game1)==0){
-                printf("ce pingouin est bloqué\n");
-            }
+            if(verify_choice_penguin(num_penguin, num_player, game1)==0){ // Vérifie si le pingouin choisi peut se déplacer
+                printf("ce pingouin est bloqué\n"); // Affiche un message si le pingouin est bloqué
+            } 
             else{
-                verify=0;
+                verify=0; // Met à jour la variable de vérification pour sortir de la boucle
             }
         }
         }while(verify);
-        choix_direction = choose_direction(num_penguin, num_player, game1);
+        choix_direction = choose_direction(num_penguin, num_player, game1); // Demande au joueur de choisir une direction de déplacement
         do{
-            printf("choisi ton nombre de deplacement");
-            scanf("%d", &nombre_deplacement);
-        }while(verify_nombre_deplacement(nombre_deplacement, choix_direction, num_penguin, num_player,  game1)==0);
-        move_total(nombre_deplacement, num_penguin, choix_direction, num_player, game1);
+            printf("choisi ton nombre de deplacement"); // Demande au joueur de choisir le nombre de déplacements
+            scanf("%d", &nombre_deplacement); // Lit le nombre de déplacements choisi
+        }while(verify_nombre_deplacement(nombre_deplacement, choix_direction, num_penguin, num_player,  game1)==0); // Vérifie si le nombre de déplacements est valide
+        move_total(nombre_deplacement, num_penguin, choix_direction, num_player, game1); // Déplace le pingouin selon les choix du joueur
         afficher_grille(game1);
-        printf("tu as gagné %d points ! \n", game1->player[num_player-1].score_player - score);
+        printf("tu as gagné %d points ! \n", game1->player[num_player-1].score_player - score); // Affiche les points gagnés par le joueur
     }
 
 }
-void all_player_tour(Game* game1){
-    int joueur = 1;
-    while(joueur!=(game1->nb_player+1)){
-        one_player_tour(joueur, game1);
-        joueur++;
+void all_player_tour(Game* game1){ // Procédure qui gère les tours de tous les joueurs dans le jeu
+    int joueur = 1; // Variable pour suivre le numéro du joueur actuel
+    while(joueur!=(game1->nb_player+1)){ // Boucle qui parcourt tous les joueurs
+        one_player_tour(joueur, game1); // Effectue le tour pour un joueur spécifique
+        joueur++; // Passe au joueur suivant
         
     }
 }
-void printf_score_player(Game* game1){
-    for(int i=0; i<game1->nb_player; i++){
-        printf("score J_%d : %d, ", i+1, game1->player[i].score_player);
+void printf_score_player(Game* game1){ // Procédure qui affiche les scores de tous les joueurs
+    for(int i=0; i<game1->nb_player; i++){ // Boucle qui parcourt tous les joueurs
+        printf("score J_%d : %d, ", i+1, game1->player[i].score_player); // Affiche le score du joueur actuel
     }
     printf("\n");
 }
-int index_max_score_player(Game* game1){
-    int max = game1->player[0].score_player;
-    int index = 1;
-    for(int i=1; i<game1->nb_player; i++){
-        if(game1->player[i].score_player> max){
-            max = game1->player[i].score_player;
-            index = i+1;
+int index_max_score_player(Game* game1){ // Fonction qui retourne l'index du joueur avec le score le plus élevé
+    int max = game1->player[0].score_player; // Initialise la variable max avec le score du premier joueur
+    int index = 1; // Initialise l'index avec 1 (pour le premier joueur)
+    for(int i=1; i<game1->nb_player; i++){ // Boucle qui parcourt tous les joueurs 
+        if(game1->player[i].score_player> max){ // Vérifie si le score du joueur actuel est supérieur au score maximal
+            max = game1->player[i].score_player; // Met à jour le score maximal avec le score du joueur actuel
+            index = i+1; // Met à jour l'index du joueur avec le score maximal
         }
     }
-    return index;
+    return index; // Retourne l'index du joueur avec le score le plus élevé
 }
-void game_total(Game* game1){
-    while(verify_all_penguin_all_player(game1)==1){
-        all_player_tour(game1);
-        printf_score_player(game1);
-        printf("Pour le moment joueur %d gagne ...\n", index_max_score_player(game1));
+void game_total(Game* game1){ // Procédure qui gère le déroulement total du jeu
+    while(verify_all_penguin_all_player(game1)==1){ // Tant que tous les joueurs ont encore des pingouins à déplacer
+        all_player_tour(game1); // Effectue le tour pour tous les joueurs
+        printf_score_player(game1); // Affiche les scores de tous les joueurs après chaque tour
+        printf("Pour le moment joueur %d gagne ...\n", index _max_score_player(game1)); // Affiche le joueur en tête jusqu'à présent
     }
-    printf("félicitation au grand gagnant joueur %d !", index_max_score_player(game1));
+    printf("félicitation au grand gagnant joueur %d !", index_max_score_player(game1)); // Une fois que tous les pingouins sont bloqués, affiche le gagnant
 }
